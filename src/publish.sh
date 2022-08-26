f5b7346d64dcdabc26f68c725811fe0916ffc025
Asif Nawaz <107853964+AsifNawaz-cnic@users.noreply.github.com>
2022-08-26 11:39:08 +0100

    Update publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -9,7 +9,7 @@ cd ".devcontainer"
 ls -altr
 docker login ghcr.io -u ${GITHUB_REF} -p ${REPO_TOKEN}
 
-#VERSION=$VERSION docker-compose -f docker-compose.yml -f $OVERRIDE up --no-start --remove-orphans
+VERSION=$VERSION docker-compose -f docker-compose.yml -f $OVERRIDE up --no-start --remove-orphans
 IMAGES=$(docker inspect --format='{{.Image}}' $(docker ps -aq))
 
 echo "IMAGES: $IMAGES"
